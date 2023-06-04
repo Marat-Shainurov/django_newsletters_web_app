b71c0797ef91b07f3c074dfd3283b5fa7d044810
Marat Shainurov <m_shaynurov@outlook.com>
2023-06-05 00:04:24 +0300

    adds and sets newsletter templates

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from newsletter.models import NewsletterSettings
+
+
+class NewsletterListView(generic.ListView):
+    model = NewsletterSettings
