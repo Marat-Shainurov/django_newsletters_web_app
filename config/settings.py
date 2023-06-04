2424f6df0cfe0ce2400bf904bfb1be9569058fdd
Marat Shainurov <m_shaynurov@outlook.com>
2023-06-04 15:02:02 +0300

    adds static, media and email settings

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,21 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'shainurov.mar@yandex.ru'
+EMAIL_HOST_PASSWORD = 'mtrqvcttesattpwj'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
