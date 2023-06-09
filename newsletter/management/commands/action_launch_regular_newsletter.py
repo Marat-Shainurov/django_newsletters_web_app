f0d37b4ecaecfb62139f9f30ae8abef55b822b34
Marat Shainurov <m_shaynurov@outlook.com>
2023-06-09 14:38:54 +0300

    updates the requirements.txt file

--- newsletter/management/commands/action_launch_regular_newsletter.py
+++ newsletter/management/commands/action_launch_regular_newsletter.py
@@ -1,9 +1,9 @@
-from crontab import CronTab
 from django.core.management import BaseCommand
 from django.shortcuts import get_object_or_404
 
 from newsletter.models import Newsletter
-from newsletter.services import send_newsletter
+
+from crontab import CronTab
 
 
 class Command(BaseCommand):
@@ -16,10 +16,17 @@ class Command(BaseCommand):
         newsletter_to_be_sent = get_object_or_404(Newsletter, pk=newsletter_id)
         newsletter_regularity = newsletter_to_be_sent.regularity
 
-        if newsletter_regularity == 'daily':
-            cron = CronTab(user=True)
-            job = cron.new(command=f'python manage.py action_send_newsletter {newsletter_id}')
-            job.minute.on(0)
-            job.hour.on(9)
-            cron.write()
-            print('Cron job was added successfully')
+        regularity_modes = {
+            'daily': '35 14 * * *',
+            'weekly': '0 12 * * 1',
+            'monthly': '0 12 1 * *'
+        }
+
+        for mode in regularity_modes:
+            if mode == newsletter_regularity:
+                cron = CronTab(user=True)
+                job = cron.new(
+                    command=f'C:\\Users\\m_sha\\AppData\\Local\\Microsoft\\WindowsApps\\python3.11.exe /path/to/django/project/manage.py action_send_newsletter {newsletter_id}')
+                job.setall(regularity_modes[mode])
+                cron.write()
+                print('Cron job is added successfully')
