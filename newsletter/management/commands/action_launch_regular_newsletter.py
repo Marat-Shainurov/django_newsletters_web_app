ce0325bf93bb4e6f666e4d37beea054a592fe3f6
Marat Shainurov <m_shaynurov@outlook.com>
2023-06-10 12:19:07 +0300

    fixes the custom cron command

--- newsletter/management/commands/action_launch_regular_newsletter.py
+++ newsletter/management/commands/action_launch_regular_newsletter.py
@@ -1,9 +1,12 @@
+import sys
+from pathlib import Path
+
+from crontab import CronTab
 from django.core.management import BaseCommand
 from django.shortcuts import get_object_or_404
 
+from config.settings import BASE_DIR
 from newsletter.models import Newsletter
-
-from crontab import CronTab
 
 
 class Command(BaseCommand):
@@ -17,18 +20,20 @@ class Command(BaseCommand):
         newsletter_regularity = newsletter_to_be_sent.regularity
 
         regularity_modes = {
-            'daily': '50 19 * * *',
+            'daily': '10 12 * * *',
             'weekly': '0 12 * * 1',
             'monthly': '0 12 1 * *'
         }
 
+        python_executable = Path(sys.executable)
+        manage_py = BASE_DIR / 'manage.py'
+
         for mode in regularity_modes:
             if mode == newsletter_regularity:
                 cron = CronTab(user=True)
-                # command = f'\\wsl$\\Ubuntu\\home\\marat_shainurov\\django_cw\\venv\\bin\\python \\wsl$\\Ubuntu\\home\\marat_shainurov\\django_cw\\manage.py action_send_newsletter {newsletter_id}'
-                # command = f'/mnt/wsl/Ubuntu/home/marat_shainurov/django_cw/venv/bin/python /mnt/wsl/Ubuntu/home/marat_shainurov/django_cw/manage.py action_send_newsletter {newsletter_id}'
-                command = f'python manage.py action_send_newsletter {newsletter_id}'
+                command = f'{python_executable} {manage_py} action_send_newsletter {newsletter_id}'
                 job = cron.new(command=command)
                 job.setall(regularity_modes[mode])
                 cron.write()
                 print('Cron job is added successfully')
+                print(f'newsletter regularity mode - {newsletter_regularity}')
