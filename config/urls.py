5540d90fd53b7ed042b47b577933acc379200219
Marat Shainurov <m_shaynurov@outlook.com>
2023-06-18 14:06:58 +0300

    adds login and logout buttons for users

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newsletter.urls', namespace='newsletter')),
-    path('clients/', include('client.urls', namespace='client'))
+    path('clients/', include('client.urls', namespace='client')),
+    path('users/', include('users.urls', namespace='users')),
 ]
--- newsletter/urls.py
+++ newsletter/urls.py
@@ -8,10 +8,10 @@ app_name = NewsletterConfig.name
 
 urlpatterns = [
     path('', NewsletterListView.as_view(), name='newsletter_list'),
-    path('create/', NewsletterCreateView.as_view(), name='newsletter_create'),
-    path('<str:slug>/', NewsletterDetailView.as_view(), name='newsletter_detail'),
-    path('update/<str:slug>/', NewsletterUpdateView.as_view(), name='newsletter_update'),
-    path('delete/<str:slug>', NewsletterDeleteView.as_view(), name='newsletter_delete'),
+    path('newsletters/create/', NewsletterCreateView.as_view(), name='newsletter_create'),
+    path('newsletters/details/<str:slug>/', NewsletterDetailView.as_view(), name='newsletter_detail'),
+    path('newsletters/update/<str:slug>/', NewsletterUpdateView.as_view(), name='newsletter_update'),
+    path('newsletters/delete/<str:slug>', NewsletterDeleteView.as_view(), name='newsletter_delete'),
     path('newsletters/attempts-report/', NewsletterAttemptsListView.as_view(), name='attempts_list'),
     path('newsletters/report/<int:pk>/', NewsletterAttemptsDetailView.as_view(), name='attempt_responses_detail')
 ]
