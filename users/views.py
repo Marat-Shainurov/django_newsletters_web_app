1607b09ba48bb8db3859332ae572d51365c69070
Marat Shainurov <m_shaynurov@outlook.com>
2023-06-18 14:38:22 +0300

    adds the register button

--- users/views.py
+++ users/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.urls import reverse_lazy
+from django.views import generic
+
+from users.forms import UserRegisterForm
+from users.models import User
+
 
 class LoginView(BaseLoginView):
     template_name = 'users/login.html'
 
 class LogoutView(BaseLogoutView):
     pass
+
+class RegisterView(generic.CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
