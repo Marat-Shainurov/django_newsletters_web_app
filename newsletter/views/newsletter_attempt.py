5960ef568d06ae2c9cf761a671b39bae7b1355c8
Marat Shainurov <m_shaynurov@outlook.com>
2023-06-26 14:24:45 +0300

    adds the newsletter filter button to the attempt_list page

--- newsletter/views/newsletter_attempt.py
+++ newsletter/views/newsletter_attempt.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from newsletter.models import NewsletterAttempts
+from newsletter.models import NewsletterAttempts, Newsletter
 
 
 class NewsletterAttemptsListView(generic.ListView):
@@ -10,7 +10,18 @@ class NewsletterAttemptsListView(generic.ListView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['page_title'] = 'Newsletter attempts'
+        context['all_newsletters'] = Newsletter.objects.all()
         return context
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset()
+
+        if 'filter_newsletter' in self.request.GET:
+            newsletter_pk = self.request.GET.get('pk_filter_newsletter')
+            newsletter = Newsletter.objects.get(pk=newsletter_pk)
+            queryset = queryset.filter(newsletter=newsletter)
+
+        return queryset
 
 
 class NewsletterAttemptsDetailView(generic.DetailView):
