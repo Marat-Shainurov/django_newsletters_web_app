c723427a551d2d3415ff595738aa2f339da62996
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-30 18:39:08 +0300

    updates newsletters urls

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -16,8 +16,6 @@ urlpatterns = [
     path('newsletters/update/<str:slug>/', NewsletterUpdateView.as_view(), name='newsletter_update'),
     path('newsletters/delete/<str:slug>', NewsletterDeleteView.as_view(), name='newsletter_delete'),
     path('newsletters/attempts-report/', NewsletterAttemptsListView.as_view(), name='attempts_list'),
-    path('newsletters/attempts-report/filter-newsletter/', NewsletterAttemptsListView.as_view(),
-         name='attempts_list_filtered_newsletter'),
     path('newsletters/reports/<int:pk>/', NewsletterAttemptsDetailView.as_view(), name='attempt_responses_detail'),
     path('newsletters/control-panel/', regular_newsletter_manager, name='control_panel'),
     path('newsletters/general-report/', regular_newsletters_report, name='regular_newsletters_report'),
@@ -25,7 +23,6 @@ urlpatterns = [
 ]
 
 # todo:
-#  filters for clients and newsletters list (city, user)
 #  Fixture
 #  Readme
 #  Docker-VM?
